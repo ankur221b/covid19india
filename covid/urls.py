2ebad2309aaf537f38ec08785d86ee08bab16c5e
Ankur Yadav <30383618+ankur221b@users.noreply.github.com>
2023-02-17 18:45:15 +0000

    latest changes

--- covid/urls.py
+++ covid/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf.urls import url,include
+from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('',include('app.urls')),
+    path('',include('app.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
