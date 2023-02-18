573ef768421aa7a02e683568b855a293f116da12
Ankur Yadav <ankur221b@gmail.com>
2023-02-18 17:07:12 +0530

    convert to node app

--- app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    return render(request,'index.html')
-
-def state(request,state):
-    return render(request,'state.html',{'state':state})
